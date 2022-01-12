9b9df33daa84eeb0e51b70895209dd943c7416f4
christinchick <alexey@skyward-crimea.ru>
2022-01-12 11:01:33 +0300

    add

---
+++ src/test/java/demoqa/PracticeForm.java
@@ -0,0 +1,45 @@
+package demoqa;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class PracticeForm {
+    @Test
+    void homeWorkTwo (){open("https://demoqa.com/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+        $("#firstName").setValue("Alex");
+        $("#lastName").setValue("Egorov");
+        $("#userEmail").setValue("alex@egorov.com");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("1231231231");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("July");
+        $(".react-datepicker__year-select").selectOption("2008");
+        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue("Maths").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).scrollTo().click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/img/1.png"));
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
+        $("#currentAddress").setValue("Simferopol");
+        $("#state").click();
+        $(byText("Haryana")).click();
+        $("#city").click();
+        $(byText("Karnal")).click();
+        $("#submit").click();
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Alex Egorov"), text("alex@egorov.com"));
+        $(".table-responsive").$(byText("Student Name"))
+                .parent().shouldHave(text("Alex Egorov"));
+    }
+}
