8085395e6c6e42260663672d81590f337c2c48b2
Christinchick <alexey@skyward-crimea.ru>
2022-02-02 10:17:42 +0300

    add Allure

--- src/test/java/testes/PracticeFormTests.java
+++ src/test/java/testes/PracticeFormTests.java
@@ -2,9 +2,14 @@ package testes;
 
 import com.github.javafaker.Faker;
 import demoqa.StartTestDemoqaRegistrationPage;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 public class PracticeFormTests extends StartTestDemoqaRegistrationPage{
     StartTestDemoqaRegistrationPage startTestDemoqaRegistrationPage = new StartTestDemoqaRegistrationPage();
@@ -15,21 +20,53 @@ public class PracticeFormTests extends StartTestDemoqaRegistrationPage{
                          currentAddress = faker.address().city(),
                          setRandomNumber = faker.numerify("##########");
 
+    @Owner("Christinchick")
+    @Feature("RegistrationForm")
+    @Story("Регистрация")
+    @DisplayName("Регистрация")
     @Test
     void fillFormTest() {
-        startTestDemoqaRegistrationPage.openPage();
-        startTestDemoqaRegistrationPage.registrationFormTestFirstName(firstName);
-        startTestDemoqaRegistrationPage.registrationFormTestLastName(lastName);
-        startTestDemoqaRegistrationPage.setGender();
-        startTestDemoqaRegistrationPage.setFakerNumber(setRandomNumber);
-        startTestDemoqaRegistrationPage.registrationUserEmail(userEmail);
-        startTestDemoqaRegistrationPage.setDate("June", "1991", "27");
-        startTestDemoqaRegistrationPage.setSubjects();
-        startTestDemoqaRegistrationPage.selectHobbies();
-        startTestDemoqaRegistrationPage.fileFile();
-        startTestDemoqaRegistrationPage.selectAddress(currentAddress);
-        startTestDemoqaRegistrationPage.chooseStateAndCity();
-        startTestDemoqaRegistrationPage.pressSubmit();
-        startTestDemoqaRegistrationPage.checkTestSubmittingForm();
+        step("Открываем страницу", () -> {
+            startTestDemoqaRegistrationPage.openPage();
+        });
+        step("Заполняем поле First Name", () -> {
+            startTestDemoqaRegistrationPage.registrationFormTestFirstName(firstName);
+        });
+        step("Заполняем поле Last Name", () -> {
+            startTestDemoqaRegistrationPage.registrationFormTestLastName(lastName);
+        });
+        step("Выбираем Gender", () -> {
+            startTestDemoqaRegistrationPage.setGender();
+        });
+        step("Вводим Mobile Namber", () -> {
+            startTestDemoqaRegistrationPage.setFakerNumber(setRandomNumber);
+        });
+        step("Заполняем поле Email", () -> {
+            startTestDemoqaRegistrationPage.registrationUserEmail(userEmail);
+        });
+        step("Выбираем Date of Birth", () -> {
+            startTestDemoqaRegistrationPage.setDate("June", "1991", "27");
+        });
+        step("Заполняем поле Subjects", () -> {
+            startTestDemoqaRegistrationPage.setSubjects();
+        });
+        step("Выбираем Hobbies", () -> {
+            startTestDemoqaRegistrationPage.selectHobbies();
+        });
+        step("Прикрепляем Picture", () -> {
+            startTestDemoqaRegistrationPage.fileFile();
+        });
+        step("Заполняем поле Current Address", () -> {
+            startTestDemoqaRegistrationPage.selectAddress(currentAddress);
+        });
+        step("Выбираем State and City", () -> {
+            startTestDemoqaRegistrationPage.chooseStateAndCity();
+        });
+        step("Кликаем кнопку Submit", () -> {
+            startTestDemoqaRegistrationPage.pressSubmit();
+        });
+        step("Проверяем введенные даные", () -> {
+            startTestDemoqaRegistrationPage.checkTestSubmittingForm();
+        });
     }
 }
