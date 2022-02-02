6fd11e3f82ec3a1f658df749420609debe8c848d
christinchick <alexey@skyward-crimea.ru>
2022-02-02 16:25:18 +0300

    add TestBase

--- src/test/java/demoqa/StartTestDemoqaRegistrationPage.java
+++ src/test/java/demoqa/StartTestDemoqaRegistrationPage.java
@@ -9,7 +9,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static testes.PracticeFormTests.*;
 
-public class StartTestDemoqaRegistrationPage {
+public class StartTestDemoqaRegistrationPage extends TestBase {
 
        public StartTestDemoqaRegistrationPage openPage() {
        open("/automation-practice-form");
